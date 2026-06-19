ac7fc2f29319f984b79a80e1456c3c99a00318f0
Vinicius <segredo.fort@gmail.com>
2026-06-18 21:29:21 -0300

    feat: cria re-export validacoes para compatibilidade com testes

--- src/utils/validacoes.js
+++ src/utils/validacoes.js
@@ -1,17 +1,4 @@
-function validarRetirada(estoqueAtual, quantidadeSolicitada) {
-  const estoque = Number(estoqueAtual);
-  const solicitada = Number(quantidadeSolicitada);
-
-  if (!Number.isFinite(estoque) || !Number.isFinite(solicitada)) {
-    return false;
-  }
-
-  if (solicitada <= 0) {
-    return false;
-  }
-
-  return solicitada <= estoque;
-}
+const { validarRetirada } = require('./validarRetirada');
 
 module.exports = {
   validarRetirada,
